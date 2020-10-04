e2aeb69413452a39b7915bcb898ce212c8d181f4
小羊 <yukifag@pm.me>
2020-10-04 19:28:22 +0700

    make it better

--- index.php
+++ index.php
@@ -52,7 +52,7 @@ require __DIR__ . "/discord.php";
     <div class="row">
     <div class="col-sm-12 col-lg-6">
 <?php
-$url                    = "https://pst.moe/paste/pzpizm?format=raw";
+$url                    = "https://unblock.my.id/cr/releases.json";
 $json                   = file_get_contents($url);
 $array                  = json_decode($json, true);
 foreach($array["releases"] as $item) {
@@ -69,7 +69,7 @@ foreach($array["releases"] as $item) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Lontong - Free Watch Anime Simulcast</title>
+<title>Lontong - Free Watch Anime</title>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
