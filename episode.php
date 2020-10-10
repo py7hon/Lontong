13ea7564f4a2ff134f03f70234a1f7b69a186ade
小羊 <yukifag@pm.me>
2020-10-10 21:06:33 +0700

    push

--- episode.php
+++ episode.php
@@ -24,6 +24,8 @@ require __DIR__ . "/discord.php";
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://bootswatch.com/4/slate/bootstrap.min.css">
+<script src="https://cdn.jsdelivr.net/npm/rrssb@1.14.0/js/rrssb.min.js" integrity="sha256-xrFsHw7ZJJpMLC2mt+vC4lrvWZjduLMR4xKpz+ICR7Q=" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/rrssb@1.14.0/css/rrssb.css" integrity="sha256-s4zpIQ3pRcSZ1dlTRAD+Rwi4lJWJGm8BuPJY6WeogTY=" crossorigin="anonymous">
 <script>function goBack() {window.history.back();}</script>
 </head>
 <body>
@@ -87,6 +89,21 @@ Lontong <img src="https://files.catbox.moe/xcncgj.png" width="30" height="30" cl
     <?php } ?>
     <hr>
     <button type="button" class="btn btn-outline-primary" onclick="goBack()">Back</button>
+    <ul class="rrssb-buttons clearfix">
+  <li class="rrssb-facebook">
+    <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $actual_link ?>" class="popup">
+      <span class="rrssb-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 29 29"><path d="M26.4 0H2.6C1.714 0 0 1.715 0 2.6v23.8c0 .884 1.715 2.6 2.6 2.6h12.393V17.988h-3.996v-3.98h3.997v-3.062c0-3.746 2.835-5.97 6.177-5.97 1.6 0 2.444.173 2.845.226v3.792H21.18c-1.817 0-2.156.9-2.156 2.168v2.847h5.045l-.66 3.978h-4.386V29H26.4c.884 0 2.6-1.716 2.6-2.6V2.6c0-.885-1.716-2.6-2.6-2.6z"/></svg></span>
+      <span class="rrssb-text">facebook</span>
+    </a>
+  </li>
+  <li class="rrssb-twitter">
+    <a href="https://twitter.com/intent/tweet?text=<?php echo $actual_link ?>"
+    class="popup">
+      <span class="rrssb-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28"><path d="M24.253 8.756C24.69 17.08 18.297 24.182 9.97 24.62a15.093 15.093 0 0 1-8.86-2.32c2.702.18 5.375-.648 7.507-2.32a5.417 5.417 0 0 1-4.49-3.64c.802.13 1.62.077 2.4-.154a5.416 5.416 0 0 1-4.412-5.11 5.43 5.43 0 0 0 2.168.387A5.416 5.416 0 0 1 2.89 4.498a15.09 15.09 0 0 0 10.913 5.573 5.185 5.185 0 0 1 3.434-6.48 5.18 5.18 0 0 1 5.546 1.682 9.076 9.076 0 0 0 3.33-1.317 5.038 5.038 0 0 1-2.4 2.942 9.068 9.068 0 0 0 3.02-.85 5.05 5.05 0 0 1-2.48 2.71z"/></svg></span>
+      <span class="rrssb-text">twitter</span>
+    </a>
+  </li>
+</ul>
     <hr>
     <div id="disqus_thread"></div>
     </div>
